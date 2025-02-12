e7f3d006c88706d8d7c1e6dfd03764130094dd15
high489 <vitalii.matskaniuk@gmail.com>
2025-02-12 19:17:41 -0300

    Update: useScroll refactoring

--- src/app/hooks/useScroll.ts
+++ src/app/hooks/useScroll.ts
@@ -1,151 +1,136 @@
-import { useCallback, useEffect, useRef, useState } from 'react'
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react'
 
-
-const useScroll = (hasScroll: boolean) => {
-  const listRef = useRef<HTMLElement>(null)
+const useScroll = (hasScroll: boolean, scrollContainerRef?: React.RefObject<HTMLElement>) => {
+  const listRef = scrollContainerRef || useRef<HTMLElement>(null)
   const scrollbarThumbRef = useRef<HTMLElement>(null)
 
   const [isDraggingScroll, setIsDraggingScroll] = useState(false)
   const [startY, setStartY] = useState(0)
   const [scrollTop, setScrollTop] = useState(0)
 
-  // calculate scrollbar thumb position
   const updateScrollThumbPosition = useCallback(() => {
-    if (listRef.current && scrollbarThumbRef.current) {
-      const scrollHeight = listRef.current.scrollHeight - listRef.current.clientHeight
-      const thumbHeight = scrollbarThumbRef.current.clientHeight
-      const maxThumbTop = scrollbarThumbRef.current.parentElement!.clientHeight - thumbHeight
-      let thumbTop = (listRef.current.scrollTop / scrollHeight) * maxThumbTop
-      thumbTop = Math.max(0, Math.min(thumbTop, maxThumbTop))
-      scrollbarThumbRef.current.style.transform = `translateY(${thumbTop}px)`
+    const list = listRef.current
+    const thumb = scrollbarThumbRef.current
+    if (list && thumb) {
+      const scrollHeight = list.scrollHeight - list.clientHeight
+      const maxThumbTop = thumb.parentElement!.clientHeight - thumb.clientHeight
+      const thumbTop = (list.scrollTop / scrollHeight) * maxThumbTop
+
+      thumb.style.transform = `translateY(${Math.max(0, Math.min(thumbTop, maxThumbTop))}px)`
     }
-  }, [])
+  }, [listRef])
 
-  // handling of scroll by mouse wheel
-  const handleWheel = useCallback((e: WheelEvent) => {
-    if (!hasScroll || !listRef.current) return
-    e.preventDefault()
-    listRef.current.scrollTop += e.deltaY
+  const handleScroll = useCallback(() => {
     updateScrollThumbPosition()
-  }, [hasScroll])
+  }, [updateScrollThumbPosition])
 
-  // handling of start touching list
-  const handleListTouchStart = useCallback((e: React.TouchEvent) => {
-    if (!hasScroll) return
-    setStartY(e.touches[0].clientY)
-    setScrollTop(listRef.current?.scrollTop || 0)
-  }, [hasScroll])
+  const handleWheel = useCallback(
+    (e: WheelEvent) => {
+      if (!hasScroll || !listRef.current) return
+      e.preventDefault()
+      listRef.current.scrollTop += e.deltaY
+      updateScrollThumbPosition()
+    },
+    [hasScroll, updateScrollThumbPosition],
+  )
 
-  // handling of scroll by touch move grabbing list
-  const handleTouchMoveByList = useCallback((e: TouchEvent) => {
-    if (!hasScroll) return
-    const selection = window.getSelection()
-    if (selection && selection.toString().length > 0) return
+  const handleListTouchStart = useCallback(
+    (e: React.TouchEvent) => {
+      if (!hasScroll) return
+      setStartY(e.touches[0].clientY)
+      setScrollTop(listRef.current?.scrollTop || 0)
+    },
+    [hasScroll, listRef],
+  )
 
-    if (listRef.current) {
+  const handleTouchMove = useCallback(
+    (e: TouchEvent) => {
+      if (!hasScroll || !listRef.current) return
       e.preventDefault()
       const deltaY = startY - e.touches[0].clientY
       listRef.current.scrollTop = Math.max(0, scrollTop + deltaY)
       updateScrollThumbPosition()
-    }
-  }, [hasScroll, startY, scrollTop])
+    },
+    [hasScroll, startY, scrollTop, updateScrollThumbPosition],
+  )
 
-  // handling of start scrolling thumb
-  const handleScrollMouseDown = useCallback((e: React.MouseEvent) => {
-    if (!hasScroll) return
-    setIsDraggingScroll(true)
-    setStartY(e.clientY)
-    setScrollTop(listRef.current?.scrollTop || 0)
-    e.preventDefault()
-  }, [hasScroll])
+  const handleScrollMouseDown = useCallback(
+    (e: React.MouseEvent | React.TouchEvent) => {
+      if (!hasScroll) return
+      setIsDraggingScroll(true)
+      setStartY(e.type === 'touchstart' ? (e as React.TouchEvent).touches[0].clientY : (e as React.MouseEvent).clientY)
+      setScrollTop(listRef.current?.scrollTop || 0)
+      e.preventDefault()
+    },
+    [hasScroll, listRef],
+  )
 
-  // handling of start touching scrolbar thumb
-  const handleScrollTouchStart = useCallback((e: React.TouchEvent) => {
-    if (!hasScroll) return
-    setIsDraggingScroll(true)
-    setStartY(e.touches[0].clientY)
-    setScrollTop(listRef.current?.scrollTop || 0)
-  }, [hasScroll])
+  const handleMouseMove = useCallback(
+    (e: MouseEvent | TouchEvent) => {
+      if (!isDraggingScroll || !listRef.current || !scrollbarThumbRef.current) return
+      e.preventDefault()
+      const deltaY =
+        (e.type === 'touchmove' ? (e as TouchEvent).touches[0].clientY : (e as MouseEvent).clientY) - startY
 
-  // handling of scroll by mouse move grabbing scrolbar thumb
-  const handleMouseMoveByScroll = useCallback((e: MouseEvent) => {
-    if (!isDraggingScroll || !listRef.current) return
-    const deltaY = e.clientY - startY
-    const scrollHeight = listRef.current.scrollHeight - listRef.current.clientHeight
-    const scrollDelta = (deltaY / scrollbarThumbRef.current!.parentElement!.clientHeight) * scrollHeight
-    listRef.current.scrollTop = Math.max(0, scrollTop + scrollDelta)
-    updateScrollThumbPosition()
-  }, [isDraggingScroll, startY, scrollTop])
+      const scrollHeight = listRef.current.scrollHeight - listRef.current.clientHeight
+      const maxThumbHeight = scrollbarThumbRef.current.parentElement!.clientHeight
+      const scrollDelta = (deltaY / maxThumbHeight) * scrollHeight
 
-  // handling of scroll by touch move grabbing scrollbar thumb
-  const handleTouchMoveByScroll = useCallback((e: TouchEvent) => {
-    if (!isDraggingScroll || !listRef.current) return
-    e.preventDefault()
-    const deltaY = e.touches[0].clientY - startY
-    const scrollHeight = listRef.current.scrollHeight - listRef.current.clientHeight
-    const scrollDelta = (deltaY / scrollbarThumbRef.current!.parentElement!.clientHeight) * scrollHeight
-    listRef.current.scrollTop = Math.max(0, scrollTop + scrollDelta)
-    updateScrollThumbPosition()
-  }, [isDraggingScroll, startY, scrollTop])
+      listRef.current.scrollTop = Math.max(0, scrollTop + scrollDelta)
+      updateScrollThumbPosition()
+    },
+    [isDraggingScroll, startY, scrollTop, updateScrollThumbPosition],
+  )
 
   const handleMouseUp = useCallback(() => {
     setIsDraggingScroll(false)
   }, [])
 
-  useEffect(() => {
+  useLayoutEffect(() => {
     if (listRef.current && scrollbarThumbRef.current) {
-      const visibleRatio = listRef.current.clientHeight / listRef.current.scrollHeight
-      const thumbHeight = Math.max(visibleRatio * listRef.current.clientHeight, 20)
-      scrollbarThumbRef.current.parentElement!.style.height = `${listRef.current.clientHeight}px`
-      scrollbarThumbRef.current.style.height = `${thumbHeight}px`
+      const list = listRef.current
+      const thumb = scrollbarThumbRef.current
+
+      const visibleRatio = list.clientHeight / list.scrollHeight
+      thumb.style.height = `${Math.max(visibleRatio * list.clientHeight, 20)}px`
       updateScrollThumbPosition()
     }
   }, [hasScroll, updateScrollThumbPosition])
 
   useEffect(() => {
-    const listElem = listRef.current
+    if (!listRef.current || !hasScroll) return
 
-    if (listElem && hasScroll) {
-      listElem.addEventListener('wheel', handleWheel, { passive: false })
-      listElem.addEventListener('touchmove', handleTouchMoveByList, { passive: false })
-    }
+    const list = listRef.current
+    list.addEventListener('scroll', handleScroll)
+    list.addEventListener('wheel', handleWheel, { passive: false })
+    list.addEventListener('touchmove', handleTouchMove, { passive: false })
 
     if (isDraggingScroll) {
-      document.addEventListener('mousemove', handleMouseMoveByScroll)
-      document.addEventListener('touchmove', handleTouchMoveByScroll, { passive: false })
+      document.addEventListener('mousemove', handleMouseMove)
+      document.addEventListener('touchmove', handleMouseMove, { passive: false })
     }
 
     document.addEventListener('mouseup', handleMouseUp)
     document.addEventListener('touchend', handleMouseUp)
 
     return () => {
-      if (listElem) {
-        listElem.removeEventListener('wheel', handleWheel)
-        listElem.removeEventListener('touchmove', handleTouchMoveByList)
-      }
-      document.removeEventListener('mousemove', handleMouseMoveByScroll)
-      document.removeEventListener('touchmove', handleTouchMoveByScroll)
+      list.removeEventListener('scroll', handleScroll)
+      list.removeEventListener('wheel', handleWheel)
+      list.removeEventListener('touchmove', handleTouchMove)
+
+      document.removeEventListener('mousemove', handleMouseMove)
+      document.removeEventListener('touchmove', handleMouseMove)
       document.removeEventListener('mouseup', handleMouseUp)
       document.removeEventListener('touchend', handleMouseUp)
     }
-  }, [
-    hasScroll,
-    isDraggingScroll,
-    handleWheel,
-    handleTouchMoveByList,
-    handleMouseMoveByScroll,
-    handleTouchMoveByScroll,
-    handleMouseUp,
-  ])
+  }, [hasScroll, isDraggingScroll, handleScroll, handleWheel, handleTouchMove, handleMouseMove, handleMouseUp])
 
   return {
     listRef,
     scrollbarThumbRef,
     handleListTouchStart,
     handleScrollMouseDown,
-    handleScrollTouchStart,
   }
-
 }
 
 export { useScroll }
--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,13 +1,10 @@
 import styles from './todo-list.module.scss'
-import { FC, RefObject, useEffect, useState } from 'react'
-
+import { FC, RefObject, useEffect, useRef, useState } from 'react'
 import { useMatchMedia, useScroll } from '@hooks'
 import { useTodoStore } from '@store'
 import { TodoListItem } from './TodoListItem'
 
-interface TodoListProps {}
-
-const TodoList: FC<TodoListProps> = () => {
+const TodoList: FC = () => {
   const { getSortedTodos, filterOption } = useTodoStore()
   const todos = getSortedTodos()
   const filteredTodos = todos.filter((todo) => {
@@ -23,39 +20,100 @@ const TodoList: FC<TodoListProps> = () => {
   const [hasScrollbar, setHasScrollbar] = useState(false)
   // should have a scroll
   const hasScroll = filteredTodos.length > 4
+
+  const listRef = useRef<HTMLUListElement>(null)
   const {
-    listRef,
     scrollbarThumbRef,
     handleListTouchStart,
     handleScrollMouseDown,
-    handleScrollTouchStart,
-  } = useScroll(hasScroll)
-  
+  } = useScroll(hasScroll, listRef)
+
   useEffect(() => {
-    if (listRef.current) setHasScrollbar(!isMobile && hasScroll)
+    setHasScrollbar(!isMobile && hasScroll)
   }, [hasScroll, isMobile])
 
   return (
-    <div className={styles['todo-list-wrapper']} >
-       <ul
-         className={styles['todo-list']}
-         ref={listRef as RefObject<HTMLUListElement>}
-         onTouchStart={handleListTouchStart}
-       >
-         {filteredTodos.map(todo => (
-           <TodoListItem key={todo.id} todo={todo}/>
-         ))}
-       </ul>
-       <div className={styles['scrollbar']} style={{ opacity: hasScrollbar ? 1 : 0}}>
-          <div
-            className={styles['scrollbar-thumb']}
-            ref={scrollbarThumbRef as RefObject<HTMLDivElement>}
-            onMouseDown={handleScrollMouseDown}
-            onTouchStart={handleScrollTouchStart}
-          ></div>
-        </div>
-     </div>
+    <div className={styles['todo-list-wrapper']}>
+      <ul
+        className={styles['todo-list']}
+        ref={listRef} 
+        onTouchStart={handleListTouchStart}
+      >
+        {filteredTodos.map(todo => (
+          <TodoListItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+      <div className={styles['scrollbar']} style={{ opacity: hasScrollbar ? 1 : 0 }}>
+        <div
+          className={styles['scrollbar-thumb']}
+          ref={scrollbarThumbRef as RefObject<HTMLDivElement>}
+          onMouseDown={handleScrollMouseDown}></div>
+      </div>
+    </div>
   )
 }
 
 export { TodoList }
+
+
+// import styles from './todo-list.module.scss'
+// import { FC, RefObject, useEffect, useState } from 'react'
+
+// import { useMatchMedia, useScroll } from '@hooks'
+// import { useTodoStore } from '@store'
+// import { TodoListItem } from './TodoListItem'
+
+// interface TodoListProps {}
+
+// const TodoList: FC<TodoListProps> = () => {
+//   const { getSortedTodos, filterOption } = useTodoStore()
+//   const todos = getSortedTodos()
+//   const filteredTodos = todos.filter((todo) => {
+//     switch (filterOption) {
+//       case 'active': return !todo.isCompleted
+//       case 'completed': return todo.isCompleted
+//       default: return todo
+//     }
+//   })
+//   // screen type
+//   const { isMobile } = useMatchMedia()
+//   // should have a scrollbar
+//   const [hasScrollbar, setHasScrollbar] = useState(false)
+//   // should have a scroll
+//   const hasScroll = filteredTodos.length > 4
+//   const {
+//     listRef,
+//     scrollbarThumbRef,
+//     handleListTouchStart,
+//     handleScrollMouseDown,
+//     handleScrollTouchStart,
+//   } = useScroll(hasScroll)
+  
+//   useEffect(() => {
+//     if (listRef.current) setHasScrollbar(!isMobile && hasScroll)
+//   }, [hasScroll, isMobile])
+
+//   return (
+//     <div className={styles['todo-list-wrapper']} >
+//        <ul
+//          className={styles['todo-list']}
+//          ref={listRef as RefObject<HTMLUListElement>}
+//          onTouchStart={handleListTouchStart}
+//        >
+//          {filteredTodos.map(todo => (
+//            <TodoListItem key={todo.id} todo={todo}/>
+//          ))}
+//        </ul>
+//        <div className={styles['scrollbar']} style={{ opacity: hasScrollbar ? 1 : 0}}>
+//           <div
+//             className={styles['scrollbar-thumb']}
+//             ref={scrollbarThumbRef as RefObject<HTMLDivElement>}
+//             onMouseDown={handleScrollMouseDown}
+//             onTouchStart={handleScrollTouchStart}
+//           ></div>
+//         </div>
+//      </div>
+//   )
+// }
+
+// export { TodoList }
